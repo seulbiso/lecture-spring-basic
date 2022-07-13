c0dda60e8f274064539b455f746b943f2b297d03
SOSEULBI <43867643+padakpadak@users.noreply.github.com>
2022-07-13 23:01:04 +0900

    [ADD] 스프링 DB JPA

--- hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,15 @@
 package hello.hellospring.domain;
 
+
+import javax.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
 
     public String getName() {
